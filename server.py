b6d6681fc235227bbb4b08b442bda244fcc5bd1f
meshanya228 <oleksyk2004@gmail.com>
2026-05-24 15:15:22 +0200

    add server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_folder="static")
+
+@app.route("/")
+def index():
+    return send_from_directory("static", "index.html")
+
+@app.route("/app")
+def webapp():
+    return send_from_directory("static", "index.html")
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}, 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
